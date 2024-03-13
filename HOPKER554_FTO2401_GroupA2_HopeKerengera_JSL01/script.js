e79cc1e3fe6a9db97843ab9a7912a34f56f3ecb7
Hope <k.hope.ing@gmail.com>
2024-03-13 14:19:55 +0200

    Testing Input Validation

--- HOPKER554_FTO2401_GroupA2_HopeKerengera_JSL01/script.js
+++ HOPKER554_FTO2401_GroupA2_HopeKerengera_JSL01/script.js
@@ -1,12 +1,27 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+    let result = ""; // Placeholder for validation result
 
-            document.getElementById('result').innerText = result;
+
+    // Check if input starts with 'pet_' and followed by alphanumeric characters
+
+    if(input.startsWith("pet_")){ //check if entered string starts with "pet_"
+        if(
+            /\d/.test(input.split("_")[1]) //check if part after "_" has numbers
+            && /[a-z]/i.test(input.split("_")[1]) && //check if part after "_" has letters
+            !/^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/.test(input.split("_")[1]) //check if part after "_" doesn't have special characters
+        ){
+            result = "Valid Syntax";  //if it meets above conditions, it is valid   
+        }
+        else{
+            result = "Invalid Syntax"; //if it doesn't then invalid
+        }
+    }
+    else{
+        result = "Invalid Syntax"; //if string doesn't start with "pet_", immediately invalid
+    }
+    document.getElementById('result').innerText = result;
 }
 
 
